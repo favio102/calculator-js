7e4fe86a6ac9263f1efacde8ff858fd89fef6d16
John Ploumistos <ploumistosjohn@gmail.com>
2023-05-09 14:17:12 +0300

    Added Max Length for Numbers,  disable buttons

--- script.js
+++ script.js
@@ -7,35 +7,66 @@ const allClearButton = document.querySelector("[data-clear-all]");
 const operand = document.querySelector("[data-operand]");
 
 function addNumber(e) {
-  let button = e.target.innerText;
-
-  operand.innerText += button;
+    let button = e.target.innerText;
+    if (operand.innerText.length >= 15) {
+        operand.innerText = "Error";
+        disableButtons();
+        deleteButton.disabled = true;
+        equalsButton.disabled = true;
+    } else {
+        operand.innerText += button;
+    }
 }
 
 function addOperation(e) {
-  let operation = e.target.innerText;
-  operand.innerText += operation;
+    let operation = e.target.innerText;
+    if (operand.innerText === "Error") {
+        return;
+    }
+
+    operand.innerText += operation;
+    if (operand.innerText === "Error") {
+        disableButtons();
+        deleteButton.disabled = false;
+        equalsButton.disabled = false;
+    }
 }
 
+function disableButtons() {
+    operationButtons.forEach((button) => {
+        button.disabled = true;
+    });
+    numberButtons.forEach((button) => {
+        button.disabled = true;
+    });
+}
 numberButtons.forEach((button) => {
-  button.addEventListener("click", addNumber);
+    button.addEventListener("click", addNumber);
 });
 
 operationButtons.forEach((operation) => {
-  operation.addEventListener("click", addOperation);
+    operation.addEventListener("click", addOperation);
 });
 
 allClearButton.addEventListener("click", () => {
-  operand.innerText = "";
+    operand.innerText = "";
+    operationButtons.forEach((button) => {
+        button.disabled = false;
+    });
+    numberButtons.forEach((button) => {
+        button.disabled = false;
+    });
+    deleteButton.disabled = false;
+    equalsButton.disabled = false;
 });
 
 deleteButton.addEventListener("click", () => {
-  operand.textContent = operand.textContent.slice(
-    0,
-    operand.textContent.length - 1
-  );
+    operand.textContent = operand.textContent.slice(
+        0,
+        operand.textContent.length - 1
+    );
 });
 
 equalsButton.addEventListener("click", () => {
-  operand.textContent = eval(operand.textContent);
+    operand.textContent = eval(operand.textContent);
 });
